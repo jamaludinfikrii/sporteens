6e73891eef53d6a0a7e45ce584a8c8a104a0ed0e
fikri <me@fikri.tech>
2020-08-28 16:03:46 +0700

    jumbotron with tagline

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,8 +6,13 @@ export class LandingPage extends Component {
         return (
             <div>
                 <div className='sporteens-bg-secondary sporteens-jumbotron p-5'>
-                    <div className="container">
-                        Ini Landing Page
+                    <div className="container h-100">
+                        <div className="row h-100 align-items-center">
+                            <div className="col-md-6 text-center text-md-left">
+                                <h1 className='sporteens-light'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                                <button className='btn btn-success mt-4'>Get It Now</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
