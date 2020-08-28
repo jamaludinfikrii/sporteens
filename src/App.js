d14e727ee6bde8e13bac830967caa1c51981362d
fikri <me@fikri.tech>
2020-08-28 14:45:36 +0700

    header responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
+import './supports/Components.css'
 
 // 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 export class Navbar extends Component {
+
+    state ={
+        openToggle : false
+    }
+
     render() {
         return (
             <div className='bg-secondary py-4'>
@@ -10,35 +15,63 @@ export class Navbar extends Component {
                     <div className="row justify-content-between px-4">
 
                         {/* Header Logo */}
-                        <div className="sporteens-logo-header ">
+                        <div className="sporteens-logo-header sporteens-clickable-el ">
                             LOGO
                         </div>
 
                         {/* Header Items */}
                         <div className="sporteens-items-header d-none d-md-block">
-                            <span className='mr-md-3'>
+                            <span className='mr-md-3 sporteens-clickable-el'>
                                 Home
                             </span>
-                            <span className='mr-md-3'>
+                            <span className='mr-md-3 sporteens-clickable-el'>
                                 Products
                             </span>
-                            <span className='mr-md-3'>
+                            <span className='mr-md-3 sporteens-clickable-el'>
                                 Brands
                             </span>
-                            <span className='mr-md-3'>
+                            <span className='mr-md-3 sporteens-clickable-el' >
                                 Cart
                             </span>   
                                
                         </div>
 
                         <div className='sporteens-items-mobile d-md-none'>
-                            <span>
-                                <FontAwesomeIcon icon={faBars}/>
-                            </span> 
+                            {
+                                this.state.openToggle ?
+                                <span onClick={ () =>this.setState({openToggle : false})} className='sporteens-clickable-el'>
+                                    <FontAwesomeIcon icon={faTimes}/>
+                                </span> 
+                                :
+                                <span onClick={ () =>this.setState({openToggle : true})} className='sporteens-clickable-el'>
+                                    <FontAwesomeIcon icon={faBars}/>
+                                </span> 
+
+                            }
                         </div>
 
 
+
                     </div>
+                    {
+                        this.state.openToggle ?
+                            <div className="sporteens-header-items-mobile px-2 d-md-none">
+                                <div className="bg-primary sporteens-clickable-el">
+                                    Home
+                                </div>
+                                <div className="bg-primary sporteens-clickable-el">
+                                    Products
+                                </div>
+                                <div className="bg-primary sporteens-clickable-el">
+                                    Brands
+                                </div>
+                                <div className="bg-primary sporteens-clickable-el">
+                                    Carts
+                                </div>
+                            </div>
+                        :
+                        null
+                    }
                 </div>
             </div>
         )
