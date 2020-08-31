bf1a7be219ca6b6a897d282106d1007e725355cd
fikri <me@fikri.tech>
2020-08-31 10:44:21 +0700

    brands bestseller banner

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import './../supports/LandingPage.css'
+import './../supports/css/LandingPage.css'
 import Image from './../supports/images/product_1.webp'
+import Logo1 from './../supports/images/logo_brand_1.png'
+import Logo2 from './../supports/images/logo_brand_2.webp'
 
 export class LandingPage extends Component {
     render() {
@@ -22,8 +24,9 @@ export class LandingPage extends Component {
                 {/* Promo Section */}
                 <div className='py-5 px-3'>
                     <div className="container mt-4 border p-3">
-                        <h3>Flash Sale</h3> 
-                        <div style={{overflow:'auto',whiteSpace:'nowrap'}} className="mt-4 pb-3">
+                        <h3 className='p-0 m-0'>Flash Sale</h3> 
+                        <p>Grab it now !!</p>
+                        <div style={{overflow:'auto',whiteSpace:'nowrap'}} className="mt-4 p-3">
                             <div className='d-inline-block mr-2' style={{width : "173px"}}>
                                 <img src={Image} width='100%'  alt="product"/>
                                 <p className='p-0 m-0 sporteens-main-dark font-weight-bold'>Product Name</p>
@@ -38,6 +41,104 @@ export class LandingPage extends Component {
 
 
                 {/* Bestseller Section */}
+
+                <div className="py-5 px-3 sporteens-bg-light-dark">
+                    <div className="container mt-4">
+                        <h3 className='p-0 m-0'>Bestseller</h3> 
+                        <p>Get our Bestseller Products This Month</p>
+
+                        <div className="row mt-4">
+                            <div className="col-6 mt-3 mt-md-0 col-md-3 ">
+                                <div className='border bg-white p-3'>
+                                    <img src={Image} width='100%'  alt="product"/>
+                                    <p className='p-0 m-0 sporteens-main-dark font-weight-bold'>Product Name</p>
+                                    <p className='p-0 m-0 text-danger'>30% Off</p>
+                                    <p className='p-0 m-0 text-secondary'> <s>Rp. 100000</s> </p>
+                                    <p className='p-0 m-0 sporteens-main-dark'> Rp. 70000 </p>
+                                </div>
+                            </div>
+                            <div className="col-6 mt-3 mt-md-0 col-md-3 ">
+                                <div className='border bg-white p-3'>
+                                    <img src={Image} width='100%'  alt="product"/>
+                                    <p className='p-0 m-0 sporteens-main-dark font-weight-bold'>Product Name</p>
+                                    <p className='p-0 m-0 text-danger'>30% Off</p>
+                                    <p className='p-0 m-0 text-secondary'> <s>Rp. 100000</s> </p>
+                                    <p className='p-0 m-0 sporteens-main-dark'> Rp. 70000 </p>
+                                </div>
+                            </div>
+                            <div className="col-6 mt-3 mt-md-0 col-md-3 ">
+                                <div className='border bg-white p-3'>
+                                    <img src={Image} width='100%'  alt="product"/>
+                                    <p className='p-0 m-0 sporteens-main-dark font-weight-bold'>Product Name</p>
+                                    <p className='p-0 m-0 text-danger'>30% Off</p>
+                                    <p className='p-0 m-0 text-secondary'> <s>Rp. 100000</s> </p>
+                                    <p className='p-0 m-0 sporteens-main-dark'> Rp. 70000 </p>
+                                </div>
+                            </div>
+                            <div className="col-6 mt-3 mt-md-0 col-md-3 ">
+                                <div className='border bg-white p-3'>
+                                    <img src={Image} width='100%'  alt="product"/>
+                                    <p className='p-0 m-0 sporteens-main-dark font-weight-bold'>Product Name</p>
+                                    <p className='p-0 m-0 text-danger'>30% Off</p>
+                                    <p className='p-0 m-0 text-secondary'> <s>Rp. 100000</s> </p>
+                                    <p className='p-0 m-0 sporteens-main-dark'> Rp. 70000 </p>
+                                </div>
+                            </div>
+                            
+                        </div>
+
+                    </div>
+                </div>
+
+
+                {/* Brands Section */}
+
+
+                <div className="py-5 px-3">
+                    <div className="container mt-4">
+                        <h3 className='p-0 m-0'>Brands</h3> 
+                        <p>We sell Official Products from various Brands</p>
+                        <div className="row mt-4  align-items-center">
+                            <div className="col-4 col-md-2   h-100">
+                                <img className='mx-2' alt='brand-logo' src={Logo1} width='100%'/>
+                            </div>
+                            <div className="col-4 col-md-2  h-100">
+                                <img className='mx-2 ' alt='brand-logo' src={Logo2} width='100%'/>
+                            </div>
+                            <div className="col-4 col-md-2   h-100">
+                                <img className='mx-2' alt='brand-logo' src={Logo1} width='100%'/>
+                            </div>
+                            <div className="col-4 col-md-2  h-100">
+                                <img className='mx-2 ' alt='brand-logo' src={Logo2} width='100%'/>
+                            </div>
+                            <div className="col-4 col-md-2   h-100">
+                                <img className='mx-2' alt='brand-logo' src={Logo1} width='100%'/>
+                            </div>
+                            <div className="col-4 col-md-2  h-100">
+                                <img className='mx-2 ' alt='brand-logo' src={Logo2} width='100%'/>
+                            </div>
+                           
+                        
+                        </div>
+                    </div>
+                </div>
+
+
+                {/* Banner Section Email Subscription */}
+                <div className="py-5 px-3">
+                    <div className="container mt-4 sporteens-bg-banner p-5 rounded">
+                        <h3 className='text-white text-center'>
+                            Subscribe to our newslater !!
+                        </h3>
+                        <p className='text-white text-center'>
+                            Get interested offer from us 
+                        </p>
+
+                        <div className='text-center'>
+                            <input type='button' className='btn btn-white' value='Subscribe Now !!' />
+                        </div>
+                    </div>
+                </div>
 
 
             </div>
