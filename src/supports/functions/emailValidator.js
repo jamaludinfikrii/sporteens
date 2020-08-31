c7acaf37430932c8a480ee51011230c8a9ce5441
fikri <me@fikri.tech>
2020-08-31 11:58:51 +0700

    validator

---
+++ src/supports/functions/emailValidator.js
@@ -0,0 +1,8 @@
+// harus ada @ 
+// ada string sebelum dan sesudah @
+// ada titik setelah string sesudah @
+// ada string setelah titik
+
+// fikri@fikri.com 
+// fikri@fikri.id
+// fikri.id@fikri.com
