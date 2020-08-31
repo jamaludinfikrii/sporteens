1cd5f294728ec2968c50f0c6e06713b7656fdf4c
fikri <me@fikri.tech>
2020-08-31 09:53:10 +0700

    flash sale section

---
+++ src/components/CardProducts.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CardProducts extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CardProducts
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './../supports/LandingPage.css'
+import Image from './../supports/images/product_1.webp'
 
 export class LandingPage extends Component {
     render() {
         return (
             <div>
+                {/* Jumbotron Section */}
                 <div className='sporteens-bg-secondary sporteens-jumbotron p-5'>
                     <div className="container h-100">
                         <div className="row h-100 align-items-center">
@@ -15,6 +17,29 @@ export class LandingPage extends Component {
                         </div>
                     </div>
                 </div>
+
+
+                {/* Promo Section */}
+                <div className='py-5 px-3'>
+                    <div className="container mt-4 border p-3">
+                        <h3>Flash Sale</h3> 
+                        <div style={{overflow:'auto',whiteSpace:'nowrap'}} className="mt-4 pb-3">
+                            <div className='d-inline-block mr-2' style={{width : "173px"}}>
+                                <img src={Image} width='100%'  alt="product"/>
+                                <p className='p-0 m-0 sporteens-main-dark font-weight-bold'>Product Name</p>
+                                <p className='p-0 m-0 text-danger'>30% Off</p>
+                                <p className='p-0 m-0 text-secondary'> <s>Rp. 100000</s> </p>
+                                <p className='p-0 m-0 sporteens-main-dark'> Rp. 70000 </p>
+                            </div>   
+                            
+                        </div>
+                    </div>
+                </div>
+
+
+                {/* Bestseller Section */}
+
+
             </div>
         )
     }
