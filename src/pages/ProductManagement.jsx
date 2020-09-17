d4086389b53104e6cad823cd11031e38c28ea45e
fikri <me@fikri.tech>
2020-09-17 09:39:19 +0700

    post data

--- src/pages/ProductManagement.jsx
+++ src/pages/ProductManagement.jsx
@@ -1,13 +1,15 @@
-import { faEdit, faSearch, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faPlus, faSearch, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Axios from 'axios'
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
 const APIURL = 'http://localhost:4000/'
 
 export class ProductManagement extends Component {
     state = {
-        data : null
+        data : null,
+        modalAddOpen : false
     }
     componentDidMount(){
         this.getAllData()
@@ -23,10 +25,56 @@ export class ProductManagement extends Component {
             console.log(err)
         })
     }
+    onAddClick = () => {
+        // ambil semua value
+        var name = this.productName.value
+        var price = this.productPrice.value
+        var images_1 = this.images1.value
+        var images_2 = this.images2.value
+        var images_3 = this.images3.value
+        var stock = this.stock.value
+        var category_id = this.categoryid.value
+
+        // cek null value
+        if(name && price && images_1 && images_2 && images_3 && stock && category_id){
+        // send value ke api
+        var data = {name,price,images_1,images_2,images_3,stock,category_id}
+        Axios.post('http://localhost:4000/product',data)
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+        
+
+        }else{
+            alert('form cannot be null')
+        }
+    }
     render() {
         return (
             <div className='container py-5'>
                 <h1>Product Management With Real API</h1>
+                <Modal toggle={() => this.setState({modalAddOpen : false})} isOpen={this.state.modalAddOpen}>
+                    <ModalHeader toggle={() => this.setState({modalAddOpen : false})}>
+                        Add New Product
+                    </ModalHeader>
+                    <ModalBody>
+                        <input type="text" ref={(el) => this.productName = el} className='form-control mt-3' placeholder='Product Name'/>
+                        <input type="number" ref={(el) => this.productPrice = el} className='form-control mt-3' placeholder='Product Price'/>
+                        <input type="text"  ref={(el) => this.images1 = el} className='form-control mt-3' placeholder='Images 1'/>
+                        <input type="text" ref={(el) => this.images2 = el} className='form-control mt-3' placeholder='Images 2'/>
+                        <input type="text" ref={(el) => this.images3 = el} className='form-control mt-3' placeholder='Images 3'/>
+                        <input type="text" ref={(el) => this.stock = el} className='form-control mt-3' placeholder='Stock'/>
+                        <input type="text" ref={(el) => this.categoryid = el} className='form-control mt-3' placeholder='Category Id'/>
+                    </ModalBody>
+                    <ModalFooter>
+                        <input type='button' onClick={this.onAddClick} className='btn btn-info' value='Add'/>
+                    </ModalFooter>
+                </Modal>
+                <FontAwesomeIcon onClick={() => this.setState({modalAddOpen : true})} icon={faPlus}/>
+
                 <table>
                     {
                         this.state.data && this.state.data.map((val,index) => {
